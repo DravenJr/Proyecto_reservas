d7043ce54c5643528668a6534672e6258b54020e
DravenJr <rivero2012x@hotmail.com>
2025-09-28 02:56:10 -0500

    Mensaje descriptivo del cambio

--- auth_service/config/settings.py
+++ auth_service/config/settings.py
@@ -52,7 +52,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'HOST': os.environ.get('DB_HOST', 'auth_service_db'),
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
