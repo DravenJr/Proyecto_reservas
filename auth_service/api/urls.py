6a2790b928dc2aa209dac4b9baa5c8b264f00a4a
DravenJr <rivero2012x@hotmail.com>
2025-09-28 02:23:10 -0500

    Mensaje descriptivo del cambio

--- auth_service/api/urls.py
+++ auth_service/api/urls.py
@@ -4,9 +4,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='auth_home'),
-    path('auth/register/', RegisterView.as_view(), name='register'),   # <- cambiado
-    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # <- cambiado
+    path('register/', RegisterView.as_view(), name='register'),   # <- cambiado
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # <- cambiado
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('me/', ProfileView.as_view(), name='profile'),
-    path('auth/dashboard/', DashboardView.as_view(), name='dashboard')  # <- cambiado
+    path('dashboard/', DashboardView.as_view(), name='dashboard')  # <- cambiado
 ]
