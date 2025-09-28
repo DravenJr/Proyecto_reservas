262b858b359f36d1c3bcf014ecd06739c6857561
DravenJr <rivero2012x@hotmail.com>
2025-09-27 19:09:35 -0500

    Corregido proxy en API Gateway y fix de botones

--- auth_service/api/urls.py
+++ auth_service/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, ProfileView, HomeView
+from .views import RegisterView, ProfileView, HomeView, DashboardView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('me/', ProfileView.as_view(), name='profile'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard')
 ]
